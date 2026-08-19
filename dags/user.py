ddcac8b7bbf89cdf984d0d97fb7105a207aed06b
Evgeniy CH <ujinwork@gmail.com>
2026-08-19 14:48:49 +0300

    assets lesson

---
+++ dags/user.py
@@ -0,0 +1,24 @@
+from airflow.sdk import asset, Asset, Context
+
+@asset(
+    schedule="@daily",
+    uri="https://randomuser.me/api/"
+)
+def user(self) -> dict[str]:
+    import requests
+
+    r = requests.get(self.uri)
+
+    return r.json()
+
+@asset(
+    schedule=user
+)
+def user_location(user: Asset, context: Context) -> dict[str]:
+    user_data = context['ti'].xcom_pull(
+        dag_id = user.name,
+        task_ids = user.name,
+        include_prior_dates = True,
+    )
+    return user_data['results'][0]['location']
+
